aa17dcefd83f673a4ed699340641566d344f8666
viniciusmotta4 <viniciusnmotta84@gmail.com>
2014-04-23 01:34:00 -0300

    Connecting with SocketIO, test

--- src/com/swphone/remoter/Main.java
+++ src/com/swphone/remoter/Main.java
@@ -2,6 +2,10 @@ package com.swphone.remoter;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import com.koushikdutta.async.http.AsyncHttpClient;
+import com.koushikdutta.async.http.socketio.ConnectCallback;
+import com.koushikdutta.async.http.socketio.SocketIOClient;
 
 public class Main extends Activity {
     /**
@@ -11,5 +15,22 @@ public class Main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    // Method that plays the video on the browser when the button 'Play' is touched.
+    public void playVideo(View view) {
+        SocketIOClient.connect(AsyncHttpClient.getDefaultInstance(), "http://www.google.com", new ConnectCallback() {
+
+            @Override
+            public void onConnectCompleted(Exception ex, SocketIOClient client) {
+                if (ex != null){
+                    ex.printStackTrace();
+                    return;
+                }
+
+                System.out.println("Connected");
+            }
+
+        });
     }
 }
