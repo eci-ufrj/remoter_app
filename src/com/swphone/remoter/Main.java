485b648a9bf2e2a0a4cdf6ddb576fe5667a559b8
Yuri <yuri.vasquez91@gmail.com>
2014-06-02 12:27:57 -0300

    Socket corrections - AndroidAsync upgrade to 1.2.6 - Given internet permission to app - Full working prototype with: -- play and stop button emiting on socket -- socket address out of code (at xml resource) -- app_socket insn't a Future<SocketIOClient> anymore, SocketIOClient now

--- src/com/swphone/remoter/Main.java
+++ src/com/swphone/remoter/Main.java
@@ -5,30 +5,36 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import com.koushikdutta.async.future.Future;
+import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.async.http.AsyncHttpClient;
 import com.koushikdutta.async.http.socketio.ConnectCallback;
 import com.koushikdutta.async.http.socketio.SocketIOClient;
+import com.koushikdutta.async.http.socketio.SocketIORequest;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Main extends Activity {
     /**
      * Called when the activity is first created.
      */
 
-    public Future<SocketIOClient> app_socket;
+    public SocketIOClient app_socket;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-        app_socket =
-                SocketIOClient.connect(AsyncHttpClient.getDefaultInstance(), "http://localhost:5000/mobile", new ConnectCallback() {
+        String socketAddress = getString(R.string.socket_address);
+        Log.d("SOCKET","Address: "+socketAddress );
+        SocketIORequest request = new SocketIORequest(socketAddress,"/mobile");
+        SocketIOClient.connect(AsyncHttpClient.getDefaultInstance(), request, new ConnectCallback() {
                     @Override
                     public void onConnectCompleted(Exception ex, SocketIOClient client) {
                         if (ex != null){
                             ex.printStackTrace();
                             return;
                         }
-
+                        app_socket = client;
                         Log.d("Connect", "Connected");
                     }
 
@@ -36,11 +42,13 @@ public class Main extends Activity {
     }
 
     // Method that plays the video on the browser when the button 'Play' is touched.
-    public void startAction(View view) {
+    public void startAction(View view) throws JSONException {
+        app_socket.emit("interaction",new JSONArray("[play_button]"));
         Log.d("Play", "Playing");
     }
 
-    public void stopAction(View view){
+    public void stopAction(View view) throws JSONException {
+        app_socket.emit("interaction",new JSONArray("[stop_button]"));
         Log.d("Stop", "Stopped");
     }
 }
